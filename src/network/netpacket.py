8bfabf6b8786522350faa226a5e7fa97b4eda83a
Weston Berg <westieberg@gmail.com>
2019-08-06 22:05:49 -0500

    Added parameter comment

--- src/network/netpacket.py
+++ src/network/netpacket.py
@@ -14,6 +14,7 @@ class NetPacket(object):
 
         :param endpoint_src: Source node
         :param endpoint_dst: Destination node
+        :param msg_payload: Encoded message to send
         """
         self.endpoint_src = endpoint_src
         self.endpoint_dst = endpoint_dst
