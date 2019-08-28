d77c7eefb61d7e0ab87b1f4c81fbdd286c21d03a
B3W <westieberg@gmail.com>
2019-08-27 20:34:03 -0500

    Second run at configuration manager. Sufficient for now.

--- src/config/config.py
+++ src/config/config.py
@@ -1,72 +1,98 @@
 '''
-Module defining methods for working with application's configuration
+Module defining logic for utilizing application's configuration
 '''
-import configparser
 import enum
+import json
 import os
 
 
 @enum.unique
 class ConfigEnum(enum.Enum):
     '''
-    Enumeration for mapping INI field strings for easy access
+    Enumeration providing friendly names for configurations
     '''
-    RX_PORT = enum.auto()    # INI Section: Communication
-    ENCODING = enum.auto()
+    RX_PORT = enum.auto()
+    BYTE_ENCODING = enum.auto()
 
 
-# Absolute path to the configuration file
-_CONFIG_FILE_PATH = os.path.abspath('config.ini')
-# Parser for reading/writing to configuration
-_parser_handle = configparser.ConfigParser()
-# Dictionary of all configuration values read in since last update
-_config_map = {}
-# Dictionary for conversion between INI field enum and string
-_key_map = {ConfigEnum.RX_PORT: 'receive_port',
-            ConfigEnum.ENCODING: 'byte_encoding'}
+# Dictionary for conversion between enum and JSON field name
+KEY_MAP = {
+    ConfigEnum.RX_PORT: 'receive_port',
+    ConfigEnum.BYTE_ENCODING: 'byte_encoding'
+}
 
 
-def get_value(key):
+class Config(object):
     '''
+    Class holding application's configuration
     '''
-    # Check key is valid and convert to INI field string
-    try:
-        key_str = _key_map[key]
-    except KeyError:
-        # TODO key not found in key map
-        raise
+    # Absolute path to the configuration file
+    _CONFIG_FILE_PATH = os.path.abspath('config.json')
+    # Handle for holding configuration data
+    _config = None
 
-    # Get value of key from configuration
-    try:
-        retval = _config_map[key_str]
-    except KeyError:
-        # TODO key not found in config
-        raise
+    @staticmethod
+    def get(key):
+        '''
+        Retrieves the configuration specified by the given key
+        '''
+        try:
+            config_value = Config._config[KEY_MAP[key]]
+        except KeyError:
+            # TODO act on error?
+            raise
 
-    return retval
+        return config_value
+
+    @staticmethod
+    def set(key, value):
+        '''
+        Sets specified configuration to specified value
+        '''
+        try:
+            Config._config[KEY_MAP[key]] = value
+        except KeyError:
+            # TODO act on error?
+            raise
+
+    @staticmethod
+    def load():
+        '''
+        Reads in configuration from disk
+        '''
+        # Validate file path
+        file_path = Config._CONFIG_FILE_PATH
+
+        if not os.path.isfile(file_path):
+            raise FileNotFoundError('Configuration file \'%s\' does not exist.'
+                                    % (file_path))
+
+        # Update configuration
+        with open(file_path, 'r') as json_file:
+            Config._config = json.load(json_file)
+
+    @staticmethod
+    def write():
+        '''
+        Writes the configuration to disk
+        '''
+        with open(Config._CONFIG_FILE_PATH, 'w') as json_file:
+            json.dump(Config._config, json_file)
 
 
-def update():
-    '''
-    Reads in configuration from disk
-    '''
-    global _parser_handle  # Globals modified within function
-    global _config_map
-
-    # TODO check the configuration file path exists
-    _parser_handle.read(_CONFIG_FILE_PATH)
-
-    for sect in _parser_handle.sections():
-        _config_map.update(_parser_handle.items(sect))
-
-
+# Testing
 if __name__ == '__main__':
-    update()
+    Config.load()
 
-    print('Configuration Map')
-    print(_config_map)
-    print()
+    print('Init Config')
+    print('RX Port: %d' % (Config.get(ConfigEnum.RX_PORT)))
+    print('Byte Encoding: %s\n' % (Config.get(ConfigEnum.BYTE_ENCODING)))
 
-    print('Configuration Settings')
-    print('RX Port: %d' % (int(get_value(ConfigEnum.RX_PORT))))
-    print('Byte Encoding: %s' % (get_value(ConfigEnum.ENCODING)))
+    Config.set(ConfigEnum.RX_PORT, 40000)
+    Config.set(ConfigEnum.BYTE_ENCODING, 'utf-16')
+
+    print('Config After Set')
+    print('RX Port: %d' % (Config.get(ConfigEnum.RX_PORT)))
+    print('Byte Encoding: %s\n' % (Config.get(ConfigEnum.BYTE_ENCODING)))
+
+    # Config.write()
