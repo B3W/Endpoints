bd75069372e3bd69514370917126627a03d51d52
B3W <westieberg@gmail.com>
2019-09-04 23:27:02 -0500

    Added utility module for shared useful functions

---
+++ src/shared/utilities.py
@@ -0,0 +1,54 @@
+'''
+Module providing various utility functions.
+'''
+import netifaces
+import socket
+
+
+def get_broadcast_ips():
+    '''
+    Determines IP addresses for brodcasting
+
+    :returns: List of broadcasting IP addresses (xxx.xxx.xxx.xxx)
+    '''
+    broadcast_ips = []  # IP addresses for broadcasting
+    skip_list = ['127.0.0.1']  # Adapter addresses to skip over
+
+    # Search through network adapters
+    for adapter in netifaces.interfaces():
+        # Get AF_INET adapter info (list of dicts of addresses)
+        af_inet_info = netifaces.ifaddresses(adapter)[netifaces.AF_INET]
+
+        for addr_dict in af_inet_info:
+            # Check AF_INET info for broadcasting address
+            try:
+                if addr_dict['addr'] not in skip_list:
+                    broadcast_ips.append(addr_dict['broadcast'])
+            except KeyError:
+                pass
+
+    return broadcast_ips
+
+
+def get_host_ip():
+    '''
+    Determines the primary address for host
+    '''
+    connection_addr = ('10.255.255.255', 1)  # Address for querying host's IP
+    sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+    try:
+        # Attempt connection
+        sock.connect(connection_addr)
+
+        # Connection successful, get IP from local socket
+        host_ip = sock.getsockname()[0]
+
+    except Exception:
+        # Fallback to loopback if no other found
+        host_ip = '127.0.0.1'
+
+    finally:
+        sock.close()
+
+    return host_ip
