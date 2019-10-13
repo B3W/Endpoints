6b9a9de95f051187d3a167a34441b49e9b3b387e
B3W <westieberg@gmail.com>
2019-10-12 23:22:01 -0500

    Fixed several bugs in and improved upon the UI design. Renamed some of the UI elements.

--- src/ui/conversationframe.py
+++ src/ui/conversationframe.py
@@ -1,18 +1,18 @@
 '''
 '''
-import messagearea as ma
+import messageframe as mf
 import tkinter as tk
 from tkinter import ttk
 
 
-class ConversationArea(ttk.Frame):
+class ConversationFrame(ttk.Frame):
     '''
     UI element displaying all relevant information of the current conversation
     '''
     _ENTRY_ROW_PAD = 20  # Padding for row containing msg entry/msg send btn
-    _ENTRY_X_PAD = (20, 0)  # 'X' pad for msg entry (l-pad, r-pad)
-    _SEND_BTN_X_PAD = (0, 20)  # 'X' pad for send btn (l-pad, r-pad)
-    _MSG_FRAME_X_PAD = (20, 20)  # 'X' pad around msg frame
+    _ENTRY_X_PAD = (10, 0)  # 'X' pad for msg entry (l-pad, r-pad)
+    _SEND_BTN_X_PAD = (0, 10)  # 'X' pad for send btn (l-pad, r-pad)
+    _MSG_FRAME_X_PAD = (0, 10)  # 'X' pad around msg frame
     _MSG_FRAME_Y_PAD = (10, 10)  # 'Y' pad around msg frame
 
     def __init__(self, master, host_id, *args, **kwargs):
@@ -20,6 +20,13 @@ class ConversationArea(ttk.Frame):
         :param master: The container holding this frame
         :param host_id: Unique id for the host running the application
         '''
+        # Configure Style
+        style = ttk.Style()
+        style.configure('tmp.TFrame', background='red')
+        style.configure('timestamp.TLabel',
+                        foreground='grey',
+                        font='Helvetica 8 italic')
+
         # Initialize frame
         ttk.Frame.__init__(self, master, *args, **kwargs)
         self.host_id = host_id
@@ -28,12 +35,12 @@ class ConversationArea(ttk.Frame):
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=0)
         self.rowconfigure(0, weight=4)
-        self.rowconfigure(1, weight=0, pad=ConversationArea._ENTRY_ROW_PAD)
+        self.rowconfigure(1, weight=0, pad=ConversationFrame._ENTRY_ROW_PAD)
 
         # MessageFrame for holding the actual messages.
         # NOTE  MessageFrames are only for displaying the messages. All info
         #       relevant to the messages/conversation is tracked elsewhere
-        self.msg_display = ma.MessageFrame(self)
+        self.msg_display = mf.MessageFrame(self)
 
         # Entry area for entering a new message
         # TODO Change to Text widget for multiline/non-text
@@ -48,26 +55,17 @@ class ConversationArea(ttk.Frame):
 
         # Place widgets
         self.msg_display.grid(column=0, row=0, columnspan=2,
-                              padx=ConversationArea._MSG_FRAME_X_PAD,
-                              pady=ConversationArea._MSG_FRAME_Y_PAD,
+                              padx=ConversationFrame._MSG_FRAME_X_PAD,
+                              pady=ConversationFrame._MSG_FRAME_Y_PAD,
                               sticky=(tk.N, tk.S, tk.E, tk.W))
 
         self.msg_entry.grid(column=0, row=1,
-                            padx=ConversationArea._ENTRY_X_PAD,
+                            padx=ConversationFrame._ENTRY_X_PAD,
                             sticky=(tk.E, tk.W))
 
         self.msg_send.grid(column=1, row=1,
-                           padx=ConversationArea._SEND_BTN_X_PAD,
+                           padx=ConversationFrame._SEND_BTN_X_PAD,
                            sticky=(tk.W,))
-
-        # Populate dummy messages
-        for i in range(30):
-            sender_id = 'None'
-            if (i % 5) == 0:
-                sender_id = 'dummy_id'
-
-            self.msg_display.add_message('item %d' % i,
-                                         sender_id == self.host_id)
 
     # CALLBACKS
     def send_message(self, event=None):
@@ -75,5 +73,5 @@ class ConversationArea(ttk.Frame):
 
         # Ignore empty messages
         if msg:
-            self.msg_display.add_message(msg, is_host=True)
+            self.msg_display.add_text_message(msg, '10/10/19 2:22 PM', True)
             self.msg_entry.delete(0, tk.END)  # Clear entry on send
---
+++ src/ui/messageframe.py
@@ -0,0 +1,116 @@
+'''
+'''
+import autoscrollbar as asb
+import math
+import message as m
+import tkinter as tk
+from tkinter import ttk
+
+
+class MessageFrame(ttk.Frame):
+    '''
+    UI element displaying the messages
+    '''
+    _SENT_X_PAD = (40, 10)  # 'X' pad for sent msg (l-pad, r-pad)
+    _SENT_STICKY = tk.E  # Side for sent messages to appear in msg frame
+    _RECV_X_PAD = (10, 40)  # 'X' pad for recv msg (l-pad, r-pad)
+    _RECV_STICKY = tk.W  # Side for received messages to appear in msg frame
+    _MSG_Y_PAD = (20, 0)  # 'Y' pad for all msg (t-pad, b-pad)
+
+    _MOUSEWHEEL_EVENT = '<MouseWheel>'  # Event to bind for mouse wheel scrolls
+
+    def __init__(self, master, *args, **kwargs):
+        '''
+        Initializes the MessageFrame
+
+        :param master: The container holding this frame
+        '''
+        self.num_msgs = 0  # Number of messages displayed
+
+        # Initialize frame holding Canvas
+        ttk.Frame.__init__(self, master, *args, **kwargs)
+        self.config(style='tmp.TFrame')
+
+        self.columnconfigure(0, weight=1)  # Canvas/Scrollable Frame
+        self.columnconfigure(1, weight=0)  # AutoScrollbar
+        self.rowconfigure(0, weight=1)
+
+        # Initialize Canvas to hold 'scrollable' frame
+        self.canvas = tk.Canvas(self, highlightthickness=0)
+        self.canvas.grid(column=0, row=0, sticky=(tk.N, tk.S, tk.E, tk.W))
+
+        # Initialize vertical AutoScrollbar and link to the Canvas
+        self.vsb = asb.AutoScrollbar(self,
+                                     column=1, row=0,
+                                     orient=tk.VERTICAL,
+                                     command=self.canvas.yview)
+
+        self.canvas.configure(yscrollcommand=self.vsb.set)
+
+        # Initialize 'scrollable' frame for actual message content
+        self.msg_frame = ttk.Frame(self.canvas)
+        self.msg_frame.columnconfigure(0, weight=1)
+
+        canvas_win_location = (0, 0)
+        self.canvas_frame_id = self.canvas.create_window(canvas_win_location,
+                                                         window=self.msg_frame,
+                                                         anchor='nw')
+
+        # Bind callbacks for when the Message Frame/Canvas is resized
+        self.msg_frame.bind('<Configure>', self.__update_scrollregion)
+        self.canvas.bind('<Configure>', self.__on_canvas_configure)
+
+        # Bind callbacks for the mouse wheel
+        self.msg_frame.bind('<Enter>', self.__bind_mousewheel)
+        self.msg_frame.bind('<Leave>', self.__unbind_mousewheel)
+
+    def add_text_message(self, text, timestamp, is_host):
+        if not text:
+            # Ignore empty messages
+            return
+
+        text_msg = m.Message(self.msg_frame, timestamp)
+        text_msg.set_text(text)
+
+        if is_host:
+            # The host sent this message
+            text_msg.grid(column=0, row=self.num_msgs,
+                          sticky=MessageFrame._SENT_STICKY,
+                          padx=MessageFrame._SENT_X_PAD,
+                          pady=MessageFrame._MSG_Y_PAD)
+
+        else:
+            # Someone other than the host sent the message
+            text_msg.grid(column=0, row=self.num_msgs,
+                          sticky=MessageFrame._RECV_STICKY,
+                          padx=MessageFrame._RECV_X_PAD,
+                          pady=MessageFrame._MSG_Y_PAD)
+
+        self.num_msgs += 1
+        self.__scroll_last_message()
+
+    def __scroll_last_message(self):
+        self.canvas.update_idletasks()  # Let canvas finish layout calculations
+        self.canvas.yview_moveto(1.0)   # Scroll to bottom of canvas
+
+    # CALLBACKS
+    def __on_canvas_configure(self, event):
+        new_canvas_width = event.width
+        self.canvas.itemconfigure(self.canvas_frame_id, width=new_canvas_width)
+
+    def __update_scrollregion(self, event=None):
+        self.canvas.configure(scrollregion=self.canvas.bbox(tk.ALL))
+
+    def __bind_mousewheel(self, event):
+        # Bind the root scrollable widget to the scrolling callback
+        self.canvas.bind_all(MessageFrame._MOUSEWHEEL_EVENT,
+                             self.__on_mousewheel)
+
+    def __unbind_mousewheel(self, event):
+        # Unbind the root scrollable widget from the scrolling callback
+        self.canvas.unbind_all(MessageFrame._MOUSEWHEEL_EVENT)
+
+    def __on_mousewheel(self, event):
+        # Get sign of delta then reverse to get scroll direction
+        scroll_dir = -1 * int(math.copysign(1, event.delta))
+        self.canvas.yview_scroll(scroll_dir, 'units')
