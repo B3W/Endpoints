d6d9bd1b58e408d5fd9d25646e85dc20371ac197
B3W <westieberg@gmail.com>
2019-10-16 23:41:27 -0500

    Updated 'Shared' package modules and reflected changes in other modules utilizing modified methods.

--- src/shared/queueprotocol.py
+++ src/shared/queueprotocol.py
@@ -7,16 +7,15 @@ import struct
 ID_FMT = '16s'
 TIMESTAMP_FMT = '16s'
 variable_fmt = '%ds'
-HEADER_FMT = ID_FMT + TIMESTAMP_FMT
-HEADER_LEN_BYTES = struct.calcsize(HEADER_FMT)
+TEXT_HEADER_FMT = ID_FMT + TIMESTAMP_FMT
+TEXT_HEADER_LEN_BYTES = struct.calcsize(TEXT_HEADER_FMT)
+CONN_HEADER_FMT = ID_FMT
+CONN_HEADER_LEN_BYTES = struct.calcsize(CONN_HEADER_FMT)
 
 
-def encode(ident, timestamp, data):
+def text_encode(ident, timestamp, data):
     '''
-    Packs data to be placed into data passing queue
-
     :param ident: bytes obj representing Endpoint's unique ID
-    :param timestamp: string representing data's timestamp
     '''
     encoded_ts = timestamp.encode()
     encoded_data = data.encode()
@@ -24,20 +23,38 @@ def encode(ident, timestamp, data):
     data_len = len(encoded_data)
     data_fmt = variable_fmt % data_len
 
-    pack_fmt = HEADER_FMT + data_fmt
+    pack_fmt = TEXT_HEADER_FMT + data_fmt
 
     return struct.pack(pack_fmt, ident, encoded_ts, encoded_data)
 
 
-def decode(byte_data):
+def text_decode(byte_data):
     '''
-    Decodes data from data passing queue
-
-    :param byte_data: Data to decode
     '''
-    header = byte_data[:HEADER_LEN_BYTES]
-    ident, raw_ts = struct.unpack(HEADER_FMT, header)
+    header = byte_data[:TEXT_HEADER_LEN_BYTES]
+    ident, raw_ts = struct.unpack(TEXT_HEADER_FMT, header)
 
-    raw_data = byte_data[HEADER_LEN_BYTES:]
+    raw_data = byte_data[TEXT_HEADER_LEN_BYTES:]
 
     return ident, raw_ts.decode(), raw_data.decode()
+
+
+def conn_encode(ident, name):
+    '''
+    :param ident: bytes obj representing Endpoint's unique ID
+    '''
+    encoded_name = name.encode()
+    name_len = len(encoded_name)
+
+    pack_fmt = CONN_HEADER_FMT + (variable_fmt % name_len)
+
+    return struct.pack(pack_fmt, ident, encoded_name)
+
+
+def conn_decode(byte_data):
+    '''
+    '''
+    ident = byte_data[:CONN_HEADER_LEN_BYTES]
+    raw_name = byte_data[CONN_HEADER_LEN_BYTES:]
+
+    return ident, raw_name.decode()
