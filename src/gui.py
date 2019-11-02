db38fcbad0be9c5edc12d5481a7266a31d1c2905
B3W <westieberg@gmail.com>
2019-11-01 22:52:37 -0500

    Module for starting up Endpoint's GUI

---
+++ src/gui.py
@@ -0,0 +1,22 @@
+'''"Frontend" for Endpoints application'''
+import logging
+import tkinter as tk
+from ui import appui
+
+
+def start(host_guid, in_q, out_q):
+    '''
+    :param in_q: Queue for data coming from backend to GUI
+    :param out_q: Queue for passing data to backend from GUI
+    '''
+    logger = logging.getLogger(__name__)  # Retrieve module logger
+
+    # Run UI
+    root = tk.Tk()
+    gui = appui.EndpointUI(root, host_guid, in_q, out_q)
+
+    logger.info('Opening UI...')
+
+    gui.mainloop()
+
+    logger.info('UI Closed')
