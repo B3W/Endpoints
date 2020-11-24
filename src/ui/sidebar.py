2ed3b0d808aa5b0d0274825c1fb696794ca7a218
B3W <westieberg@gmail.com>
2020-11-23 23:19:13 -0600

    Swapped Tk listbox for custom listbox

--- src/ui/sidebar.py
+++ src/ui/sidebar.py
@@ -1,16 +1,10 @@
 '''
 Module providing the side panel UI elements.
 '''
-import autoscrollbar as asb
+import connectionwidget as cw
+import customlistbox as clb
 import tkinter as tk
 from tkinter import ttk
-
-
-class Connection(object):
-    '''Class defining all data related to a connection'''
-    def __init__(self, guid, name):
-        self.ident = guid
-        self.name = name
 
 
 class SideBar(ttk.Frame):
@@ -32,35 +26,13 @@ class SideBar(ttk.Frame):
         self.activate_callback = activate_callback
         self.remove_callback = remove_callback
 
-        # List of active connections
-        self.connections = []
-
         # Root frame grid
-        self.columnconfigure(0, weight=1)  # Listbox col
-        self.columnconfigure(1, weight=0)  # Scrollbar col
-        self.rowconfigure(0, weight=1)  # Listbox row
-        self.rowconfigure(1, weight=0)  # Scrollbar row
-
-        # Create and place scrollbars
-        self._vscroll = asb.AutoScrollbar(self, column=1, row=0,
-                                          orient=tk.VERTICAL)
-        self._hscroll = asb.AutoScrollbar(self, column=0, row=1,
-                                          orient=tk.HORIZONTAL)
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
 
         # Create and place listbox
-        self.listbox = tk.Listbox(self,
-                                  selectmode=tk.BROWSE,
-                                  activestyle=tk.NONE,
-                                  relief=tk.FLAT,
-                                  highlightthickness=0,
-                                  yscrollcommand=self._vscroll.set,
-                                  xscrollcommand=self._hscroll.set)
-
-        self.listbox.grid(column=0, row=0, sticky=(tk.N, tk.S, tk.E, tk.W))
-
-        # Link scrollbars with listbox
-        self._vscroll.config(command=self.listbox.yview)
-        self._hscroll.config(command=self.listbox.xview)
+        self.listbox = clb.CustomListbox(self, style='Listbox.TFrame')
+        self.listbox.grid(column=0, row=0, sticky=tk.NSEW)
 
         self.listbox.bind('<<ListboxSelect>>', self.__on_connection_select)
 
@@ -75,8 +47,11 @@ class SideBar(ttk.Frame):
         self.add_callback(ident, conn_name)
 
         # Add connection to sidebar
-        self.connections.append(Connection(ident, conn_name))
-        self.listbox.insert(tk.END, conn_name)
+        widget = cw.ConnectionWidget(self.listbox.widget_frame,
+                                     ident,
+                                     conn_name)
+
+        self.listbox.insert(tk.END, widget)
 
     def remove_connection(self, ident):
         '''
@@ -84,64 +59,39 @@ class SideBar(ttk.Frame):
 
         :param ident: ID of connection to remove
         '''
-        # Get the name associated with the ID
-        index = self._find_connection_by_id(ident)
+        # Get the widget associated with the ID
+        index = -1
+        counter = 0
+
+        for widget in self.listbox.get(0, tk.END):
+            if widget.guid == ident:
+                index = counter
+
+            counter += 1
 
         if index == -1:
             # ID not found
             return
 
-        try:
-            connection = self.connections[index]
-            item_index = self.listbox.get(0, tk.END).index(connection.name)
+        # Remove connection from sidebar
+        self.listbox.delete(index)
 
-            # Remove connection from sidebar
-            self.connections.remove(connection)
-            self.listbox.delete(item_index)
-
-            # Remove conversation from ConversationFrame
-            self.remove_callback(ident)
-
-        except ValueError:
-            # Connection not found
-            pass
-
-    def _find_connection_by_id(self, ident):
-        index = -1
-
-        for i in range(len(self.connections)):
-            if self.connections[i].ident == ident:
-                index = i
-                break
-
-        return index
-
-    def _find_connection_by_name(self, name):
-        index = -1
-
-        for i in range(len(self.connections)):
-            if self.connections[i].name == name:
-                index = i
-                break
-
-        return index
+        # Remove conversation from ConversationFrame
+        self.remove_callback(ident)
 
     # CALLBACKS
     def __on_connection_select(self, event):
         widget = event.widget
 
+        # Determine connection to activate
         try:
-            # Determine connection to activate
             index = int(widget.curselection()[0])
 
         except IndexError:
             # 'curselection' returned empty list
             return
 
-        conn_name = widget.get(index)
+        connection = widget.get(index)
 
         # Notify ConversationFrame of activation
-        index = self._find_connection_by_name(conn_name)
-
-        if index >= 0:
-            self.activate_callback(self.connections[index].ident)
+        self.activate_callback(connection.ident)
