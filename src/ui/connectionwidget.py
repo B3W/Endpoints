76e53b3ffe9f7d33051d9f47d58e69d40d7eec5d
B3W <westieberg@gmail.com>
2020-11-25 23:48:51 -0600

    Added unread notification functionality

--- src/ui/connectionwidget.py
+++ src/ui/connectionwidget.py
@@ -11,6 +11,8 @@ class ConnectionWidget(tk.Button):
         # Initialize root widget
         tk.Button.__init__(self, master,
                            text=name,
+                           anchor=tk.W,
+                           compound=tk.RIGHT,
                            relief=tk.FLAT,
                            background='white',
                            padx=5,
@@ -20,6 +22,8 @@ class ConnectionWidget(tk.Button):
         self.guid = guid
         self.name = name
         self.selected = False
+        self.notify_pending = True
+        self.notify_image = None
 
         self.bind('<Button-1>', self.__click_callback)
         self.bind('<Enter>', self.__hover_enter)
@@ -29,9 +33,23 @@ class ConnectionWidget(tk.Button):
         self.selected = True
         self.configure(background='light sky blue')
 
+        if self.notify_pending:
+            self.notify_pending = False
+            self.notify_image = None
+            self.configure(image='')
+
     def deselect(self):
         self.selected = False
         self.configure(background='white')
+
+    def notify(self):
+        if self.selected:
+            return
+
+        self.notify_pending = True
+        notify_image_path = 'assets/Unread-Notification-16x16.png'
+        self.notify_image = tk.PhotoImage(file=notify_image_path)
+        self.configure(image=self.notify_image)
 
     # CALLBACKS
     def __click_callback(self, event):
